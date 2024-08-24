7a96f66b1eb20de9c8e59bfa9bfe383b9a25dafb
trulacgc <trulacgc.tcnetwork@gmail.com>
2024-08-24 12:27:07 +0700

    Mint 100 ERC-20 tokens

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,7 @@ async function main() {
    * @dev make sure the first argument has the same name as your contract in the Hello_swtr.sol file
    * @dev the second argument must be the message we want to set in the contract during the deployment process
    */
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik!!"]);
+  const contract = await hre.ethers.deployContract("TestToken");
 
   await contract.waitForDeployment();
 
---
+++ scripts/mint.js
@@ -0,0 +1,37 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0x493be891f2f3Ef4E239B6043503c0D8B16A06162";
+  const [signer] = await hre.ethers.getSigners();
+
+  const contractFactory = await hre.ethers.getContractFactory("TestToken");
+  const contract = contractFactory.attach(contractAddress);
+
+  const functionName = "mint100tokens";
+  const mint100TokensTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName),
+    0
+  );
+
+  await mint100TokensTx.wait();
+
+  console.log("Transaction Receipt: ", mint100TokensTx.hash);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
